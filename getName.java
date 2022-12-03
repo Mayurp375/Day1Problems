3946b2ff16f3dc73654458f69d65379f983a4378
Mayurp375 <mayurpolojwar1234@gmail.com>
2022-12-03 12:33:57 +0530

    get the name using the command line.

---
+++ getName.java
@@ -0,0 +1,10 @@
+package Day1Problem;
+
+public class getName {
+    public static void main(String[] args) {
+
+        for (int i = 0; i < args.length; i++) {
+            System.out.println(args[i]);
+        }
+    }
+}
