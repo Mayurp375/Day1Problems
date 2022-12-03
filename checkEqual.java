6f5e775276b2e08ad3d0f3b4dafba594904ed064
Mayurp375 <mayurpolojwar1234@gmail.com>
2022-12-03 10:35:26 +0530

    check Equal

---
+++ checkEqual.java
@@ -0,0 +1,14 @@
+package Day1Problem;
+
+public class checkEqual {
+    public static void main(String[] args) {
+        String string = "Mayur";
+        String string2 = "Polojwar";
+
+        if (string2.equals(string)) {
+            System.out.println("is Equal");
+        } else {
+            System.out.println("is not Equal");
+        }
+    }
+}
